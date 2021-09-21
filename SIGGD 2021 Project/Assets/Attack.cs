1f661945d13a50db8883d53f6a0dfcf25414f694
ryan-doan <doanngocdung01@gmail.com>
2021-09-21 15:33:57 -0400

    a

--- SIGGD 2021 Project/Assets/Attack.cs
+++ SIGGD 2021 Project/Assets/Attack.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     public Transform attackHitbox;
-    public float range = .5f;
+    public float range = .8f;
     public LayerMask enemyLayers;
+
+    float delay = .5f;
+    float timeElapsed = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,18 +18,25 @@ public class Attack : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (timeElapsed > delay)
         {
-            Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(attackHitbox.position, range, enemyLayers);
-
-            foreach(Collider2D enemy in enemiesHit)
+            if (Input.GetKey(KeyCode.Space))
             {
-                Debug.Log("We hit" + enemy.name);
-            }
+                Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(attackHitbox.position, range, enemyLayers);
 
-            
+                foreach (Collider2D enemy in enemiesHit)
+                {
+                    Debug.Log("We hit" + enemy.name);
+                }
+
+                timeElapsed = 0;
+            }
+        }
+        else if (timeElapsed < delay)
+        {
+            timeElapsed += Time.deltaTime;
         }
     }
 
