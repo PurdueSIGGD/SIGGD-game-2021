073c663e15362c211f7a5b082ee872f27a8b024a
Tom Yarrow <tyarrow1@gmail.com>
2021-09-24 19:35:26 -0400

    Door improvements, they currently function

--- SIGGD 2021 Project/Assets/Scripts/Environment/Door.cs
+++ SIGGD 2021 Project/Assets/Scripts/Environment/Door.cs
@@ -4,30 +4,42 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    public bool open = false;
+    public bool startOpen = false;
+    private bool open = false;
 
-    // # of seconds to open the door
+    // # of seconds to open/close the door (currently unused)
     public float openDelay = 0.5f;
-
-    // # of seconds to close the door
     public float closeDelay = 0.5f;
 
-    // Game Event to trigger when opened, closed
+    // Optional Game Event to trigger when opened, closed (if you want the door to trigger something else)
     public GameEvent openEvent, closeEvent;
     SpriteRenderer sprite;
-
 
     // Start is called before the first frame update
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
+
+        // Open or close the door so its state
+        if (startOpen != open)
+        {
+            if (open)
+            {
+                CloseInternal();
+            } else
+            {
+                OpenInternal();
+            }
+        }
     }
 
     // Starts/finishes the opening process. Called by events.
     public void Open()
     {
-        open = true;
+        
         openEvent?.Invoke();
+
+        OpenInternal();
     }
 
     // Starts/finishes the closing process. Called by events.
@@ -35,6 +47,22 @@ public class Door : MonoBehaviour
     {
         open = false;
         closeEvent?.Invoke();
+
+        CloseInternal();
+    }
+
+    // Opens the door without triggering events. Used for resetting.
+    private void OpenInternal()
+    {
+        // Just disable the sprite right now
+        sprite.enabled = false;
+    }
+
+    // Closes the door without triggering events. Used for resetting.
+    private void CloseInternal()
+    {
+        // Just re-enable the sprite right now
+        sprite.enabled = true;
     }
 
     // Toggles the door. Called by events.
