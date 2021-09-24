3bcfdc71a42e7eb6577f5e5cfe46bca57d91c2fd
Andy Sharpe <ajsusa1@gmail.com>
2021-09-24 19:48:18 -0400

    Ray fixes

--- SIGGD 2021 Project/Assets/Scripts/Environment/rayDetecor.cs
+++ SIGGD 2021 Project/Assets/Scripts/Environment/rayDetecor.cs
@@ -6,16 +6,55 @@ public class rayDetecor : MonoBehaviour
     private GameEvent eventToCall;
 
     [SerializeField]
+    private bool onEnter = true;
+
+    [SerializeField]
+    private bool onStay = true;
+
+    [SerializeField]
+    private bool onExit = true;
+
+    [SerializeField]
+    private float start = 1;
+
+    [SerializeField]
     private float length = 2;
 
     [SerializeField]
     private LayerMask layers;
 
+    private bool staying = false;
+
     private void Update()
     {
-        if (Physics2D.Raycast(transform.position, transform.forward, length, layers))
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + transform.right * start, transform.right, length, layers);
+        if (hit && hit.transform != transform)
         {
-            eventToCall.Invoke();
+            if (staying)
+            {
+                if (onStay)
+                {
+                    eventToCall.Invoke();
+                }
+            } else
+            {
+                if (onEnter)
+                {
+                    eventToCall.Invoke();
+                }
+                staying = true;
+            }
+        }
+        else
+        {
+            if (staying)
+            {
+                if (onExit)
+                {
+                    eventToCall.Invoke();
+                }
+                staying = false;
+            }
         }
     }
 }
