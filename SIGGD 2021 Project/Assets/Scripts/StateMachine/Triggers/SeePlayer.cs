2574c387c1fafd35181d4ebe669070478984b755
Andrew Choi <choi681@purdue.edu>
2021-10-19 16:12:53 -0400

    alert enemy AI implemented

--- SIGGD 2021 Project/Assets/Scripts/StateMachine/Triggers/SeePlayer.cs
+++ SIGGD 2021 Project/Assets/Scripts/StateMachine/Triggers/SeePlayer.cs
@@ -7,12 +7,11 @@ public class SeePlayer : Trigger
     public Transform player;
     public bool NOT = false; //if true then SeePlayer returns if the enemy can NOT see the player
     [SerializeField] private Transform eyeTransform;
+    [SerializeField] private LayerMask layerMask;
     public override bool isActive()
     {
         bool ret = NOT;
-        LayerMask enemyMask = (int)Mathf.Pow(2, 7);
-        LayerMask layerMask = ~enemyMask; //ignore collisions with the enemy
-        RaycastHit2D hit = Physics2D.Linecast(eyeTransform.position, player.position); //draw a line between the enemy and the player
+        RaycastHit2D hit = Physics2D.Linecast(eyeTransform.position, player.position, layerMask); //draw a line between the enemy and the player
         if (hit.transform.gameObject.CompareTag("Player")) //if the line hits the player
         {
             Debug.DrawLine(eyeTransform.position, player.position, Color.green);
