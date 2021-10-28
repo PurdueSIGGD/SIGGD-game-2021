e27eaa5a63fab6e729aa271ee96c52d7f1bf471a
Tom Yarrow <tyarrow1@gmail.com>
2021-10-27 20:01:33 -0400

    Test Item and start on inventory icon script

---
+++ SIGGD 2021 Project/Assets/Inventory.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // Currently equipped item
+    public GameObject equippedItem;
+}
---
+++ SIGGD 2021 Project/Assets/Scripts/InventoryGraphics.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryGraphics : MonoBehaviour
+{
+    // Sprites for when the inventory item is currently able to be used or not.
+    [SerializeField] private Sprite enabledSprite;
+    [SerializeField] private Sprite disabledSprite;
+
+    // UI element references for displayed parts
+    [SerializeField] private Image borderImage;
+    [SerializeField] private Image itemImage;
+    [SerializeField] private Text stackText;
+
+    private Inventory inv;
+
+    private void Start()
+    {
+        inv = GetComponent<Inventory>();
+    }
+
+    private void Update()
+    {
+        UpdateDisplayedItem();
+    }
+
+    // Used for when the displayed item needs to be updated
+    public void UpdateDisplayedItem()
+    {
+        if (!inv.equippedItem)
+        {
+            // Remove any icon
+            return;
+        }
+
+        // Recalculate the item sprite
+        ICanUse item = inv.equippedItem.GetComponent<ICanUse>();
+        
+        if (item is Stackable)
+        {
+            Debug.Log("Item is stackable to size "+((Stackable)item).count);
+        } else
+        {
+            Debug.Log("Item is not stackable");
+        }
+    }
+}
