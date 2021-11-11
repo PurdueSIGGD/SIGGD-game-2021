7c4410217dbd28d9014f20649264098d699d3dd6
WorseluckJohn <84751789+WorseluckJohn@users.noreply.github.com>
2021-11-10 19:59:40 -0500

    Audible Tiles

---
+++ SIGGD 2021 Project/Assets/AudibleTile.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudibleTile : MonoBehaviour
+{
+    public bool isTriggered = false;
+    public Material triggeredMaterial;
+    public AudioSource triggerSound;
+    private CircleCollider2D soundCollider;
+    public int radius = 3;
+
+    private void Start()
+    {
+        soundCollider = gameObject.GetComponent<CircleCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<isGrounded>().grounded && !isTriggered)
+        {
+            isTriggered = true;
+            gameObject.GetComponent<SpriteRenderer>().material = triggeredMaterial;
+        }
+    }
+
+    IEnumerator timer()
+    {
+        soundCollider.radius = radius;
+        yield return new WaitForSecondsRealtime(.3f);
+        soundCollider.radius = 0;
+    }
+}
