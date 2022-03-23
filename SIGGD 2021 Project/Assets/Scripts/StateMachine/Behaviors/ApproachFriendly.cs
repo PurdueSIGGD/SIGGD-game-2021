ab9cdbad7fe049f98c739cb746d848c084104a7a
andyjsharpe <ajsusa1@gmail.com>
2022-03-23 19:38:15 -0400

    Close to done

--- SIGGD 2021 Project/Assets/Scripts/StateMachine/Behaviors/ApproachFriendly.cs
+++ SIGGD 2021 Project/Assets/Scripts/StateMachine/Behaviors/ApproachFriendly.cs
@@ -2,22 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ApproachPlayer : Behavior
+public class ApproachFriendly : Behavior
 {
-    public Transform player;
+    public Transform friendly;
     private Vector2 oldPos;
     [SerializeField] private NavmeshAgent navmeshAgent;
 
     public float approachSpeed = 1f;
+
     public override void run()
     {
         //this is to reduce the munber of path recalculations
-        Vector2 newPos = player.position;
+        Vector2 newPos = friendly.position;
         if (Mathf.Pow(newPos.x - oldPos.x, 2) + Mathf.Pow(newPos.y - oldPos.y, 2) > 1)
         {
             oldPos = newPos;
             navmeshAgent.navigateTo(newPos);
-        } else
+        }
+        else
         {
             navmeshAgent.navigateTo(oldPos);
         }
@@ -25,7 +27,7 @@ public class ApproachPlayer : Behavior
 
     public override void OnBehaviorEnter()
     {
-        
+
     }
     public override void OnBehaviorExit()
     {
