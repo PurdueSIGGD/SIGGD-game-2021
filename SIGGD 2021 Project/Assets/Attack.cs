1911c7ec8c533b5477e09d08932236677b896089
Tom Yarrow <tyarrow1@gmail.com>
2022-04-11 19:54:45 -0400

    Player/Enemy damage sounds (Death/Hit WIP)

--- SIGGD 2021 Project/Assets/Attack.cs
+++ SIGGD 2021 Project/Assets/Attack.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Attack : MonoBehaviour
 {
     [SerializeField] private LayerMask enemyLayers;
+    [SerializeField] private UnityEvent attackEvent;
 
     [SerializeField] private float offset = 1f;
     [SerializeField] private float range = .5f;
@@ -28,6 +30,8 @@ public class Attack : MonoBehaviour
                 }
 
                 Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(transform.position + lastMovement * offset, range, enemyLayers);
+
+                attackEvent.Invoke();
 
                 foreach (Collider2D enemy in enemiesHit)
                 {
