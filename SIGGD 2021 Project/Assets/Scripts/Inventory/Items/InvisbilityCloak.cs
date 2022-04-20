1a1e3a31e8137cd33c0f7dfbda00b0bdff0baf1c
Tom Yarrow <tyarrow1@gmail.com>
2022-04-20 19:15:30 -0400

    Invisibility cloak updates

--- SIGGD 2021 Project/Assets/Scripts/Inventory/Items/InvisbilityCloak.cs
+++ SIGGD 2021 Project/Assets/Scripts/Inventory/Items/InvisbilityCloak.cs
@@ -10,22 +10,31 @@ public class InvisbilityCloak : MonoBehaviour
     [SerializeField]
     private string playerLayerName = "Player";
 
+    [SerializeField]
+    List<SpriteRenderer> visibilityList;
+
     GameObject playerObject;
 
     private void Start()
     {
         playerObject = transform.parent.parent.gameObject;
+        visibilityList.AddRange(playerObject.GetComponentsInChildren<SpriteRenderer>());
     }
 
     public void StartInvisibility()
     {
         playerObject.layer = LayerMask.NameToLayer(invisibleLayerName);
-        playerObject.GetComponent<SpriteRenderer>().enabled = false;
+        foreach (SpriteRenderer sr in visibilityList) {
+            sr.enabled = false;
+        }
     }
 
     public void EndInvisibility()
     {
         playerObject.layer = LayerMask.NameToLayer(playerLayerName);
-        playerObject.GetComponent<SpriteRenderer>().enabled = true;
+        foreach (SpriteRenderer sr in visibilityList)
+        {
+            sr.enabled = true;
+        }
     }
 }
